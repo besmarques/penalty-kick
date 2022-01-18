141bca30701ed30a79eb10d38d906cd272b0759a
Bruno Marques <besmarques@gmail.com>
2022-01-18 21:53:18 +0000

    penalty kick working

---
+++ src/js/component/penalty.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+let directions = ["TL", "TC", "TR", "ML", "MC", "MR", "BL", "BC", "BR"];
+let kickResult = "teste";
+
+function goalOrNot(direction) {
+	//alert(direction);
+	let gk = directions[Math.floor(Math.random() * directions.length)];
+	//alert(gk);
+
+	if (direction != gk) {
+		kickResult = "GOAL!!!!!!!!!";
+	} else {
+		kickResult = "DEFENSE!!!!!!";
+	}
+	alert(kickResult);
+}
+
+const Penalty = () => {
+	return (
+		<>
+			<p>Penalty</p>
+			<div className="container">
+				<div className="goal">
+					<div className="row d-flex justify-content-center g-0">
+						<div className="col-4">
+							<button onClick={() => goalOrNot("TL")}></button>
+						</div>
+						<div className="col-4">
+							<button onClick={() => goalOrNot("TC")}></button>
+						</div>
+						<div className="col-4">
+							<button onClick={() => goalOrNot("TR")}></button>
+						</div>
+					</div>
+					<div className="row d-flex justify-content-center g-0">
+						<div className="col-4">
+							<button onClick={() => goalOrNot("ML")}></button>
+						</div>
+						<div className="col-4">
+							<button onClick={() => goalOrNot("MC")}></button>
+						</div>
+						<div className="col-4">
+							<button onClick={() => goalOrNot("MR")}></button>
+						</div>
+					</div>
+					<div className="row d-flex justify-content-center g-0">
+						<div className="col-4">
+							<button onClick={() => goalOrNot("BL")}></button>
+						</div>
+						<div className="col-4">
+							<button onClick={() => goalOrNot("BC")}></button>
+						</div>
+						<div className="col-4">
+							<button onClick={() => goalOrNot("BR")}></button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div>
+				<div>{kickResult}</div>
+			</div>
+		</>
+	);
+};
+
+export default Penalty;
